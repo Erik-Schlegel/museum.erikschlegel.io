c19bbdb066f4cab94e2262a8ed0e2f955b84a54e
Erik Schlegel <epschlegel@gmail.com>
2022-06-11 02:00:16 -0700

    steps into SPA

---
+++ client/Scripts/main.js
@@ -0,0 +1,38 @@
+
+(() => {
+	let placeholderEl;
+
+	window.onload = async () => {
+		placeholderEl = document.querySelector('[data-id=ContentPlaceholder]');
+
+		LoadAndShowContent(placeholderEl);
+
+		window.addEventListener('hashchange', () => {
+			LoadAndShowContent(placeholderEl);
+		});
+	}
+})()
+
+
+const LoadAndShowContent = async (placeholderEl) => {
+	let content = await LoadContent(GetRoute());
+	ShowContent(placeholderEl, content);
+}
+
+
+const GetRoute = () => {
+	let route = window.location.hash.substr(1);
+	return route || 'home';
+}
+
+
+const LoadContent = async (namedTemplate) => {
+	let content = await fetch(`./Templates/${namedTemplate}.html`);
+	let contentText = await content.text();
+	return contentText;
+}
+
+
+const ShowContent = (el, content) => {
+	el.innerHTML = content;
+}
